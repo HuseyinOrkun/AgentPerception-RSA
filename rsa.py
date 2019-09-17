6d73d2e2adf5903d114ebbac68074fa73f6e091f
huseyinelmas <huseyinorkunelmas@gmail.com>
2019-09-17 14:40:13 +0300

    - Added sena's correlate_models function - Changed logic for similarity

--- rsa.py
+++ rsa.py
@@ -1,9 +1,11 @@
 from scipy.spatial.distance import pdist,squareform
 import scipy.io as sio
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import seaborn as sns
 import pandas as pd
+from scipy import stats
+
 # Input: X ndarray, An m by n array of m original observations in an n-dimensional space.
 # metric: A difference metric to compare activation patterns from {'hamming', 'mahalanobis'}
 # TODO: to add more distances, add args and kwargs to signature.
@@ -17,11 +19,11 @@ def create_rdm(X, metric, name):
     # The metric dist(u=X[i], v=X[j]) is computed and stored in entry ij of RDM
     RDM = pdist(X, metric)
     RDM = squareform(RDM)
-    numpy.save(name+'RDM', RDM)
+    np.save(name+'RDM', RDM)
     fig, ax = plt.subplots()
 
     colorData = sio.loadmat('colorData.mat')
-    cmap = numpy.flipud(colorData['Blues9'])
+    cmap = np.flipud(colorData['Blues9'])
     cmap = cmap[1:,:]
     ax = sns.heatmap(RDM, cmap=cmap.tolist())
     plt.savefig(name+'RDM.png')
@@ -38,16 +40,47 @@ def find_maximal_correlation(EEG_RDM_dict, model_RDM):
 
     dist_per_time_window = []
     for time_window, EEG_RDM in EEG_RDM_dict.items():
+        kendall_tau, kendall_p_value = correlate_models(model_RDM, EEG_RDM)
+        dist_per_time_window.append([time_window, kendall_tau, kendall_p_value])
+    time_window_dist_df = pd.DataFrame(dist_per_time_window, columns=['time_window','kendall_tau','kendall_p_value'])
 
-        # TODO change this function to Sena's function (if it is similarity change here accordingly)
-        dist = numpy.linalg.norm(EEG_RDM - model_RDM)
-        dist_per_time_window.append([time_window,dist])
-    time_window_dist_df = pd.DataFrame(dist_per_time_window, columns=['time_window','Distance'])
-
-    # if metric is similarity instead of distance (dissimilarity), change ascending to True
-    time_window_dist_df_sorted = time_window_dist_df.sort_values(by='Distance')
+    # if metric is similarity instead of distance (dissimilarity), change ascending to False
+    time_window_dist_df_sorted = time_window_dist_df.sort_values(by='kendall_tau', ascending=False)
 
     return time_window_dist_df_sorted.iloc[0], time_window_dist_df
+
+
+# Should we really concat upper and lower triangles
+# the correlation doubles
+def correlate_models(model_rdm, eeg_rdm):
+
+    # upper triangle. k=1 excludes the diagonal elements.
+    xu, yu = np.triu_indices_from(model_rdm, k=1)
+
+    # lower triangle
+    xl, yl = np.tril_indices_from(model_rdm, k=-1)  # Careful, here the offset is -1
+
+    # combine
+    x = np.concatenate((xl, xu))
+    y = np.concatenate((yl, yu))
+    off_model_rdm = model_rdm[x,y]
+
+    # upper triangle. k=1 excludes the diagonal elements.
+    xu, yu = np.triu_indices_from(eeg_rdm, k=1)
+    # lower triangle
+    xl, yl = np.tril_indices_from(eeg_rdm, k=-1)  # Careful, here the offset is -1
+
+    # combine
+    x = np.concatenate((xl, xu))
+    y = np.concatenate((yl, yu))
+    off_eeg_rdm = eeg_rdm[x,y]
+
+    # Kendall’s tau is a measure of the correspondence between two rankings. Values
+    # close to 1 indicate strong agreement, values close to -1 indicate strong disagreement.
+    # The two-sided p-value for a hypothesis test whose null hypothesis is an absence of
+    # association, i.e. tau = 0.
+    kendall_tau, kendall_p_value = stats.kendalltau(off_model_rdm, off_eeg_rdm)
+    return kendall_tau, kendall_p_value
 
 if __name__ == '__main__':
     robot_drink = [1,0,0]
@@ -81,14 +114,14 @@ if __name__ == '__main__':
                android_drink, android_grasp, android_handwave, android_talk, android_nudge, android_paper, android_turn, android_wipe,
                human_drink, human_grasp, human_handwave, human_talk, human_nudge, human_paper, human_turn, human_wipe]
 
-    stimuli = numpy.asarray(stimuli)
+    stimuli = np.asarray(stimuli)
     model_RDM = create_rdm(stimuli, 'hamming', 'Agent')
 
     EEG_rand_dict = {}
 
     # Simulating an EEG rdm list with random RDMs
     for i in range(1,10):
-        EEG_rand_dict[str(i)] = (numpy.random.rand(24, 24))
+        EEG_rand_dict[str(i)] = (np.random.rand(24, 24))
 
     # Putting the model RDM to show that it will be returned as most similar
     EEG_rand_dict['11'] = model_RDM
