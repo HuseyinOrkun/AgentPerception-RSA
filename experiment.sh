cd92f75a7e43b117d8728a06ecad409464f1f2c2
valkyrie7 <iamotley7@gmail.com>
2020-07-23 14:26:16 +0300

    experiment.sh minor change

--- experiment.sh
+++ experiment.sh
@@ -20,7 +20,7 @@ naive_still_sv_path="/auto/data2/oelmas/RSA_Results/Naive/Still_MF/"
 model_path="/auto/data2/oelmas/RSA_Models/"
 stimuli_type="still-mf"
 experiment_type="naive"
-python rsa_main.py $naive_still_in_path $model_path $time_window $naive_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > naive_still_MF_out.txt
+python rsa_main.py $naive_still_in_path $model_path $time_window $naive_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_still_MF_out.txt
 
 # FIRST-FRAME STILL
 naive_still_in_path="/auto/data2/oelmas/EEG_AgentPerception_NAIVE/Still_FF/"
@@ -28,7 +28,7 @@ naive_still_sv_path="/auto/data2/oelmas/RSA_Results/Naive/Still_FF/"
 model_path="/auto/data2/oelmas/RSA_Models/"
 stimuli_type="still-ff"
 experiment_type="naive"
-python rsa_main.py $naive_still_in_path $model_path $time_window $naive_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > naive_still_FF_out_sep_subj.txt
+python rsa_main.py $naive_still_in_path $model_path $time_window $naive_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_still_FF_out.txt
 
 # VIDEO
 naive_video_in_path="/auto/data2/oelmas/EEG_AgentPerception_NAIVE/Video/"
@@ -36,7 +36,7 @@ naive_video_sv_path="/auto/data2/oelmas/RSA_Results/Naive/Video/"
 model_path="/auto/data2/oelmas/RSA_Models/"
 stimuli_type="video"
 experiment_type="naive"
-python rsa_main.py $naive_video_in_path $model_path $time_window $naive_video_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > naive_video_out_sep_subj.txt
+python rsa_main.py $naive_video_in_path $model_path $time_window $naive_video_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_video_out.txt
 
 
 ########################################
@@ -49,7 +49,7 @@ prior_still_sv_path="/auto/data2/oelmas/RSA_Results/Prior/Still/"
 model_path="/auto/data2/oelmas/RSA_Models/"
 stimuli_type="still-ff"
 experiment_type="prior"
-python rsa_main.py $prior_still_in_path $model_path $time_window $prior_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > prior_still_out_sep_subj.txt
+python rsa_main.py $prior_still_in_path $model_path $time_window $prior_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/prior_still_out.txt
 
 # VIDEO
 prior_video_in_path="/auto/data2/oelmas/EEG_AgentPerception_Prior/Video/"
@@ -57,5 +57,5 @@ prior_video_sv_path="/auto/data2/oelmas/RSA_Results/Prior/Video/"
 model_path="/auto/data2/oelmas/RSA_Models/"
 stimuli_type="video"
 experiment_type="prior"
-python rsa_main.py $prior_video_in_path $model_path $time_window $prior_video_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > prior_video_out_sep_subj.txt
+python rsa_main.py $prior_video_in_path $model_path $time_window $prior_video_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/prior_video_out.txt
 
