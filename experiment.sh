298e8b374238a3c053368439d6739f8a0d827382
valkyrie7 <iamotley7@gmail.com>
2020-08-29 18:45:41 +0300

    small change to bash file

--- experiment.sh
+++ experiment.sh
@@ -22,6 +22,7 @@ stimuli_type="still-mf"
 experiment_type="naive"
 python rsa_main.py $naive_still_in_path $model_path $time_window $naive_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_still_MF_out.txt
 
+echo "V1 NAIVE MID-FRAME STILL DONE"
 # FIRST-FRAME STILL
 naive_still_in_path="/auto/data2/oelmas/EEG_AgentPerception_NAIVE/Still_FF/"
 naive_still_sv_path="/auto/data2/oelmas/RSA_Results_V1/Naive/Still_FF/"
@@ -29,12 +30,15 @@ stimuli_type="still-ff"
 experiment_type="naive"
 python rsa_main.py $naive_still_in_path $model_path $time_window $naive_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_still_FF_out.txt
 
+echo "V1 NAIVE FIRST-FRAME STILL DONE"
+
 # VIDEO
 naive_video_in_path="/auto/data2/oelmas/EEG_AgentPerception_NAIVE/Video/"
 naive_video_sv_path="/auto/data2/oelmas/RSA_Results_V1/Naive/Video/"
 stimuli_type="video"
 experiment_type="naive"
 python rsa_main.py $naive_video_in_path $model_path $time_window $naive_video_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_video_out.txt
+echo "V1 NAIVE VIDEO DONE"
 
 
 ########################################
@@ -48,6 +52,7 @@ stimuli_type="still-ff"
 experiment_type="prior"
 python rsa_main.py $prior_still_in_path $model_path $time_window $prior_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/prior_still_out.txt
 
+echo "V1 PRIOR STILL DONE"
 # VIDEO
 prior_video_in_path="/auto/data2/oelmas/EEG_AgentPerception_Prior/Video/"
 prior_video_sv_path="/auto/data2/oelmas/RSA_Results_V1/Prior/Video/"
@@ -55,6 +60,7 @@ stimuli_type="video"
 experiment_type="prior"
 python rsa_main.py $prior_video_in_path $model_path $time_window $prior_video_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/prior_video_out.txt
 
+echo  "V1 PRIOR VIDEO DONE"
 
 
 ############## V2 ####################
@@ -75,12 +81,15 @@ stimuli_type="still-mf"
 experiment_type="naive"
 python rsa_main.py $naive_still_in_path $model_path $time_window $naive_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_still_MF_out.txt
 
+echo "V2 NAIVE MID-FRAME STILL DONE"
+
 # FIRST-FRAME STILL
 naive_still_in_path="/auto/data2/oelmas/EEG_AgentPerception_NAIVE/Still_FF/"
 naive_still_sv_path="/auto/data2/oelmas/RSA_Results_V2/Naive/Still_FF/"
 stimuli_type="still-ff"
 experiment_type="naive"
 python rsa_main.py $naive_still_in_path $model_path $time_window $naive_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_still_FF_out.txt
+echo "V2 NAIVE FIRST-FRAME STILL DONE"
 
 # VIDEO
 naive_video_in_path="/auto/data2/oelmas/EEG_AgentPerception_NAIVE/Video/"
@@ -89,6 +98,7 @@ stimuli_type="video"
 experiment_type="naive"
 python rsa_main.py $naive_video_in_path $model_path $time_window $naive_video_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/naive_video_out.txt
 
+echo "V2 NAIVE VIDEO DONE"
 
 ########################################
 #           PRIOR EXPERIMENT           #
@@ -100,6 +110,7 @@ prior_still_sv_path="/auto/data2/oelmas/RSA_Results_V2/Prior/Still/"
 stimuli_type="still-ff"
 experiment_type="prior"
 python rsa_main.py $prior_still_in_path $model_path $time_window $prior_still_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/prior_still_out.txt
+echo "V2 PRIOR STILL DONE"
 
 # VIDEO
 prior_video_in_path="/auto/data2/oelmas/EEG_AgentPerception_Prior/Video/"
@@ -107,4 +118,6 @@ prior_video_sv_path="/auto/data2/oelmas/RSA_Results_V2/Prior/Video/"
 stimuli_type="video"
 experiment_type="prior"
 python rsa_main.py $prior_video_in_path $model_path $time_window $prior_video_sv_path $eeg_distance $model_distance $experiment_type $stimuli_type --avg_subjects_rdm > outputs/prior_video_out.txt
+
+echo "V2 PRIOR VIDEO DONE"
 
